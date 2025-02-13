77a1129287959d66ce77a53db3645792989e3ce9
Marlon-S <marlonsuyuc@gmail.com>
2025-02-12 18:28:14 -0600

    implement product pagination with image retrieval and pagination component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,23 +1,36 @@
-import { ProductGrid, Title } from "@/components";
-// import { useProductsStore } from "@/store";
-import { initialData } from "../../seed/seed";
+export const revalidate = 60
+// false | 0 | number
 
-export default function Home() {
-  const products = initialData.products;
-  // const products = useProductsStore.getState().products
+import { ProductGrid, Title } from "@/components"
+import { getPaginatedProductsWithImages } from "@/actions/products/product-pagination"
+import { redirect } from "next/navigation"
+import Pagination from "@/components/ui/pagination/Pagination"
+// import { initialData } from "../../seed/seed";
+// const products = initialData.products;
 
-  // const fetchData = useProductsStore.getState().fetchData
-  // fetchData()
+// TODO: link documentation to the searchParams: https://nextjs.org/docs/app/building-your-application/upgrading/version-15#async-request-apis-breaking-change
+
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
+
+export default async function Home(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams
+  // console.log(searchParams)
+  const page = searchParams.page ? Number(searchParams.page) : 1
+
+  const { products, totalPages } = await getPaginatedProductsWithImages({
+    page,
+  })
+
+  // console.log(currentPage, totalPages);
+  if (products.length === 0) {
+    redirect("/")
+  }
 
   return (
     <>
-      <Title
-        title="Best Sellers
-"
-        subTitle="All products"
-        className="mb-2"
-      />
+      <Title title="Best Sellers" subTitle="All products" className="mb-2" />
       <ProductGrid products={products} />
+      <Pagination totalPages={totalPages} />
     </>
-  );
+  )
 }
