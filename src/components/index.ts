ed0389e4f3fbbf9c9ea9891fd7db856ae165fa71
marlon <marlonsuyuc@gmail.com>
2025-04-04 13:37:25 -0600

    Refactor import statements for consistency and clarity

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,17 @@
-export * from './products/product-grid/ProductGrid'
-export * from './products/product-grid/ProductGridItem'
+export * from "./products/product-grid/ProductGrid";
+export * from "./products/product-grid/ProductGridItem";
 
-export * from './ui/not-found/PageNotFound'
-export * from './ui/sidebar/Sidebar'
-export * from './ui/title/Title'
-export * from './ui/top-menu/TopMenu'
+export * from "./ui/not-found/PageNotFound";
+export * from "./ui/sidebar/Sidebar";
+export * from "./ui/title/Title";
+export * from "./ui/top-menu/TopMenu";
 
-export * from './product/size-selector/SizeSelector'
-export * from './product/quantity-selector/QuantitySelector'
+export * from "./product/size-selector/SizeSelector";
+export * from "./product/quantity-selector/QuantitySelector";
 
-export * from './product/slideshow/ProductMobileSlideshow'
-export * from './product/slideshow/ProductSlideshow'
+export * from "./product/slideshow/ProductMobileSlideshow";
+export * from "./product/slideshow/ProductSlideshow";
 
-export * from './ui/footer/Footer'
+export * from "./ui/footer/Footer";
 
-export * from './provider/Provider'
+export * from "./providers/Providers";
