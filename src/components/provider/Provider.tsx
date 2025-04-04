058169c37d0abe244180f86519578af81f1a2e56
marlon <marlonsuyuc@gmail.com>
2025-04-04 13:34:32 -0600

    Refactor provider component to integrate PayPalScriptProvider and replace SessionProvider

--- src/components/provider/Provider.tsx
+++
@@ -1,8 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-
-interface Props {
-  children: React.ReactNode;
-}
-export default function Provider({ children }: Props) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
